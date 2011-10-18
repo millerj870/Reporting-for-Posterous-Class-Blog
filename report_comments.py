b29912c241e735c26318c5c759f931dc9109067e
Jason Miller <millerj870@gmail.com>
2011-10-18 13:54:59 -0500

    added README and COPYING.txt files

--- report_comments.py
+++ report_comments.py
@@ -1,3 +1,7 @@
+# This work is copyright 2011, Jason Miller. All rights reserved except as permitted under the Creative Commons Attribution-NonCommercial
+# 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send a letter to Creative
+# Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. #
+
 """
 This report returns the following information about posts made to spectrawiki.posterous.com:
 	name of comment author
@@ -14,7 +18,7 @@ d=datetime.datetime.today().strftime("%y%B%d-%H%M")
 filename = 'report-comments-' + str(d) +'.txt' 
 log=open(filename,'w')
 
-for post in Cursor(method=api.read_posts, start_page=1, parameters={'hostname':'spectrawiki'}):
+for post in Cursor(method=api.read_posts, start_page=1, parameters={'hostname':'[name]'}):
 	try:
 		for comment in post.comments:
 			print >> log, "%s, %s, %s, %s" % (comment.author, comment.date, post.title, post.url)
--- report_posts.py
+++ report_posts.py
@@ -1,3 +1,7 @@
+# This work is copyright 2011, Jason Miller. All rights reserved except as permitted under the Creative Commons Attribution-NonCommercial
+# 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send a letter to Creative
+# Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. # 
+
 """
 This report returns the following information about posts made to spectrawiki.posterous.com:
 	name of author
@@ -17,7 +21,7 @@ d=datetime.datetime.today().strftime("%y%B%d-%H%M")
 filename = 'report-posts-' + str(d) +'.txt' 
 log=open(filename,'w')
 
-for post in Cursor(method=api.read_posts, start_page=1, parameters={'hostname':'spectrawiki'}):
+for post in Cursor(method=api.read_posts, start_page=1, parameters={'hostname':'[name]'}):
 	try:
 #		print >> log,  "--------------------"
 		print >> log, "%s, %s, %s, %s" % (post.author, post.date, post.title, post.url)
